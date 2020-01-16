105a18b809ed88c35c768d36e14d272514e9632e
Ricardo González Moreno <correoricky@gmail.com>
2020-01-16 14:27:57 +0100

    Refs #7294. Support colcon builds.

--- src/test/java/com/eprosima/fastrtps/FastRTPSGenTest.java
+++ src/test/java/com/eprosima/fastrtps/FastRTPSGenTest.java
@@ -27,49 +27,18 @@ public class FastRTPSGenTest
     @Test
     public void runTests()
     {
-        if(!isUnix())
+        if (!isUnix())
         {
             System.out.println("WARNING: The tests are only available with an unix system");
             return;
         }
 
-        // Get client's branch against test will compile.
-        String branch = System.getProperty("branch");
-        if(branch == null || branch.isEmpty())
-        {
-            branch = "master";
-        }
-
-        //Configure Fast-RTPS for the tests
-        ArrayList<String[]> commands = new ArrayList<String[]>();
-        if (Files.notExists(Paths.get(OUTPUT_PATH + "/Fast-RTPS/build")))
-        {
-            commands.add(new String[]{"mkdir -p " + OUTPUT_PATH, "."});
-            commands.add(new String[]{"rm -rf Fast-RTPS", OUTPUT_PATH});
-            commands.add(new String[]{"git clone -b " + branch + " https://github.com/eProsima/Fast-RTPS.git", OUTPUT_PATH});
-            commands.add(new String[]{"mkdir build", OUTPUT_PATH + "/Fast-RTPS"});
-        }
-        else
-        {
-            commands.add(new String[]{"git checkout -f " + branch, OUTPUT_PATH + "/Fast-RTPS"});
-        }
-        commands.add(new String[]{"cmake .. -DTHIRDPARTY=ON -DCMAKE_INSTALL_PREFIX=install", OUTPUT_PATH + "/Fast-RTPS/build"});
-        commands.add(new String[]{"make -j" + Runtime.getRuntime().availableProcessors() + " install", OUTPUT_PATH + "/Fast-RTPS/build"});
-
-        for(String[] command: commands)
-        {
-            if(!Command.execute(command[0], command[1], true))
-            {
-                System.exit(-1);
-            }
-        }
-
         //Configure idl tests
-        TestManager tests = new TestManager(TestLevel.RUN, "share/fastrtps/fastrtpsgen", INPUT_PATH, OUTPUT_PATH + "/idls", "CMake");
-        tests.addCMakeArguments("-DCMAKE_PREFIX_PATH=" + System.getProperty("user.dir") + "/" + OUTPUT_PATH + "/Fast-RTPS/build/install");
-        //tests.addCMakeArguments("-DCMAKE_PREFIX_PATH=" + "/home/lgasco/git/eProsima/Fast-RTPS/master/build/install");
+        TestManager tests = new TestManager(TestLevel.RUN, "share/fastrtps/fastrtpsgen", INPUT_PATH,
+                        OUTPUT_PATH + "/idls", "CMake");
         tests.removeTests(IDL.ARRAY_NESTED, IDL.SEQUENCE_NESTED);
         boolean testResult = tests.runTests();
         System.exit(testResult ? 0 : -1);
     }
+
 }
