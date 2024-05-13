99ef2c3c7ccfa758c39e4e4b0102f882b1ed7fe5
Mario Dominguez <mariodominguez@eprosima.com>
2024-05-13 14:23:42 +0200

    Refs #20755: import java.nio.file.Paths
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>

--- src/main/java/com/eprosima/fastdds/fastddsgen.java
+++ src/main/java/com/eprosima/fastdds/fastddsgen.java
@@ -42,6 +42,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
