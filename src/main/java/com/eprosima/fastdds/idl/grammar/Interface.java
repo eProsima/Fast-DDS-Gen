adc08d56e28f33d4192f49c10dd967921df1bce1
Miguel Company <miguelcompany@eprosima.com>
2025-04-07 16:59:46 +0200

    Generate ServiceTypeSupport for interfaces (#459)
    * Refs #22996. Skeleton for TypesCdrAuxHeaderImpl.
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    * Refs #22996. Added code to get reply type.
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    * Refs #22996. Added Operation `getOutTypeCode` and `getResultTypeCode`
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    * Refs #22996. Added typecode for Exception.
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    * Refs #22996. Generate serialization code for replies.
    
    * Refs #22996. Generate serialization code for operation inputs.
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    * Refs #22996. Generate serialization code for input feeds.
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    * Refs #22996. Generate serialization code for requests.
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    * Refs #22996. Generate code in PubSubTypeHeader.
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    * Refs #22996. Skeleton for code generation in PubSubTypeSource.
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    * Refs #22996. Code generation in PubSubTypeSource.
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    ---------
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>

--- src/main/java/com/eprosima/fastdds/idl/grammar/Interface.java
+++ src/main/java/com/eprosima/fastdds/idl/grammar/Interface.java
@@ -15,6 +15,11 @@
 package com.eprosima.fastdds.idl.grammar;
 
 import com.eprosima.fastdds.idl.grammar.Operation;
+import com.eprosima.fastdds.idl.parser.typecode.EnumTypeCode;
+import com.eprosima.fastdds.idl.parser.typecode.StructTypeCode;
+import com.eprosima.idl.parser.typecode.Member;
+import com.eprosima.idl.parser.typecode.EnumMember;
+import com.eprosima.idl.parser.typecode.TypeCode.ExtensibilityKind;
 import org.antlr.v4.runtime.Token;
 
 public class Interface extends com.eprosima.idl.parser.tree.Interface
@@ -44,5 +49,100 @@ public class Interface extends com.eprosima.idl.parser.tree.Interface
         super.add(exp);
     }
 
-    private Context m_context;
+    /*!
+     * @brief This function is used in stringtemplates to generate the typesupport code for the interface.
+     *
+     * @return The typecode of the request type.
+     */
+    public StructTypeCode getRequestTypeCode()
+    {
+        if (m_request_type == null)
+        {
+            String scope = getHasScope() ? getScope() + "::detail" : "detail";
+            StructTypeCode request_type = new StructTypeCode(scope, getName() + "_Request");
+
+            // The request type should be a `@choice`, which means that it should have MUTABLE extensibility,
+            // and also treat all fields as optional.
+            request_type.get_extensibility(ExtensibilityKind.MUTABLE);
+
+            getAll_operations().forEach(operation -> {
+                Operation op = (Operation)operation;
+                Member member = new Member(op.getInTypeCode(), op.getName());
+                // Add `@optional` and `@hashid` annotations
+                member.addAnnotation(m_context, new Annotation(m_context.getAnnotationDeclaration("optional")));
+                member.addAnnotation(m_context, new Annotation(m_context.getAnnotationDeclaration("hashid")));
+                request_type.addMember(member);
+                // Add input feed parameters
+                op.getParameters().forEach(param -> {
+                    Param p = (Param)param;
+                    if (p.isInput() && p.isAnnotationFeed())
+                    {
+                        Member feed_member = new Member(p.getFeedTypeCode(), op.getName() + "_" + p.getName());
+                        feed_member.addAnnotation(m_context, new Annotation(m_context.getAnnotationDeclaration("optional")));
+                        feed_member.addAnnotation(m_context, new Annotation(m_context.getAnnotationDeclaration("hashid")));
+                        request_type.addMember(feed_member);
+                    }
+                });
+            });
+
+            m_request_type = request_type;
+        }
+        return m_request_type;
+    }
+
+    /*!
+     * @brief This function is used in stringtemplates to generate the typesupport code for the interface.
+     *
+     * @return The typecode of the reply type.
+     */
+    public StructTypeCode getReplyTypeCode()
+    {
+        if (m_reply_type == null)
+        {
+            String scope = getHasScope() ? getScope() + "::detail" : "detail";
+            StructTypeCode reply_type = new StructTypeCode(scope, getName() + "_Reply");
+
+            // The reply type should be a `@choice`, which means that it should have MUTABLE extensibility,
+            // and also treat all fields as optional.
+            reply_type.get_extensibility(ExtensibilityKind.MUTABLE);
+
+            getAll_operations().forEach(operation -> {
+                Operation op = (Operation)operation;
+                Member member = new Member(op.getResultTypeCode(), op.getName());
+                // Add `@optional` and `@hashid` annotations
+                member.addAnnotation(m_context, new Annotation(m_context.getAnnotationDeclaration("optional")));
+                member.addAnnotation(m_context, new Annotation(m_context.getAnnotationDeclaration("hashid")));
+                reply_type.addMember(member);
+            });
+
+            Member remoteEx = new Member(get_remoteExceptionCode_t_type(), "remoteEx");
+            remoteEx.addAnnotation(m_context, new Annotation(m_context.getAnnotationDeclaration("optional")));
+            remoteEx.addAnnotation(m_context, new Annotation(m_context.getAnnotationDeclaration("hashid")));
+            reply_type.addMember(remoteEx);
+
+            m_reply_type = reply_type;
+        }
+        return m_reply_type;
+    }
+
+    private EnumTypeCode get_remoteExceptionCode_t_type()
+    {
+        if (m_remoteExceptionCode_t_type == null)
+        {
+            EnumTypeCode type = new EnumTypeCode("eprosima::fastdds::dds::rpc", "RemoteExceptionCode_t");
+            type.addMember(new EnumMember("REMOTE_EX_OK"));
+            type.addMember(new EnumMember("REMOTE_EX_UNSUPPORTED"));
+            type.addMember(new EnumMember("REMOTE_EX_INVALID_ARGUMENT"));
+            type.addMember(new EnumMember("REMOTE_EX_OUT_OF_RESOURCES"));
+            type.addMember(new EnumMember("REMOTE_EX_UNKNOWN_OPERATION"));
+            type.addMember(new EnumMember("REMOTE_EX_UNKNOWN_EXCEPTION"));
+            m_remoteExceptionCode_t_type = type;
+        }
+        return m_remoteExceptionCode_t_type;
+    }
+
+    private Context m_context = null;
+    private StructTypeCode m_request_type = null;
+    private StructTypeCode m_reply_type = null;
+    static private EnumTypeCode m_remoteExceptionCode_t_type = null;
 }
--- src/main/java/com/eprosima/fastdds/idl/grammar/Operation.java
+++ src/main/java/com/eprosima/fastdds/idl/grammar/Operation.java
@@ -14,9 +14,14 @@
 
 package com.eprosima.fastdds.idl.grammar;
 
+import com.eprosima.fastdds.idl.parser.typecode.StructTypeCode;
+import com.eprosima.fastdds.idl.grammar.Param;
 import com.eprosima.idl.parser.exception.RuntimeGenerationException;
 import com.eprosima.idl.parser.exception.ParseException;
-import com.eprosima.fastdds.idl.grammar.Param;
+import com.eprosima.idl.parser.typecode.Member;
+import com.eprosima.idl.parser.typecode.Kind;
+import com.eprosima.idl.parser.typecode.PrimitiveTypeCode;
+import com.eprosima.idl.parser.typecode.TypeCode.ExtensibilityKind;
 import org.antlr.v4.runtime.Token;
 
 public class Operation extends com.eprosima.idl.parser.tree.Operation
@@ -49,6 +54,17 @@ public class Operation extends com.eprosima.idl.parser.tree.Operation
         return false;
     }
 
+    /**
+     * Return whether the operation has been annotated as mutable
+     *
+     * @return true when the operation has been annotated as mutable, false otherwise.
+     */
+    public boolean isAnnotationMutable()
+    {
+        // TODO(MiguelCompany): Add support for @mutable annotation
+        return false;
+    }
+
     @Override
     public void add(com.eprosima.idl.parser.tree.Param param)
     {
@@ -71,5 +87,159 @@ public class Operation extends com.eprosima.idl.parser.tree.Operation
         super.add(param);
     }
 
-    private Context m_context;
+    public StructTypeCode getInTypeCode()
+    {
+        if (m_in_type == null)
+        {
+            Interface parent = (Interface)getParent();
+            String scope = parent.getHasScope() ? parent.getScope() + "::detail" : "detail";
+
+            // Create In type
+            StructTypeCode in_type = new StructTypeCode(
+                scope,
+                parent.getName() + "_" + getName() + "_In");
+
+            // If the operation is marked as mutable, then the in type should be mutable as well
+            if (isAnnotationMutable())
+            {
+                in_type.get_extensibility(ExtensibilityKind.MUTABLE);
+            }
+            else
+            {
+                in_type.get_extensibility(ExtensibilityKind.FINAL);
+            }
+
+            // Add non-feed input parameters as members
+            getParameters().forEach(param -> {
+                if (param.isInput() && !((Param)param).isAnnotationFeed())
+                {
+                    Member member = new Member(param.getTypecode(), param.getName());
+                    in_type.addMember(member);
+                }
+            });
+
+            m_in_type = in_type;
+        }
+        return m_in_type;
+    }
+
+    public StructTypeCode createFeedTypeCode(Param p)
+    {
+        // Add feed typecode to the parameter
+        Interface iface = (Interface)getParent();
+        String scope = iface.getHasScope() ? iface.getScope() + "::detail" : "detail";
+
+        StructTypeCode feed_type = new StructTypeCode(
+            scope,
+            iface.getName() + "_" + getName() + "_" + p.getName() + "_Feed");
+
+        feed_type.get_extensibility(ExtensibilityKind.FINAL);
+
+        // Add optional value member
+        Member value_member = new Member(p.getTypecode(), "value");
+        value_member.addAnnotation(m_context, new Annotation(m_context.getAnnotationDeclaration("optional")));
+        feed_type.addMember(value_member);
+
+        // Add optional finished_ member
+        Member finished_member = new Member(m_context.createPrimitiveTypeCode(Kind.KIND_LONG), "finished_");
+        finished_member.addAnnotation(m_context, new Annotation(m_context.getAnnotationDeclaration("optional")));
+        feed_type.addMember(finished_member);
+
+        return feed_type;
+    }
+
+    public StructTypeCode getOutTypeCode()
+    {
+        if (m_out_type == null)
+        {
+            Interface parent = (Interface)getParent();
+            String scope = parent.getHasScope() ? parent.getScope() + "::detail" : "detail";
+
+            // Create Out type
+            StructTypeCode out_type = new StructTypeCode(
+                scope,
+                parent.getName() + "_" + getName() + "_Out");
+
+            // If the operation is marked as mutable, then the out type should be mutable as well
+            if (isAnnotationMutable())
+            {
+                out_type.get_extensibility(ExtensibilityKind.MUTABLE);
+            }
+            else
+            {
+                out_type.get_extensibility(ExtensibilityKind.FINAL);
+            }
+
+            if (isAnnotationFeed())
+            {
+                // Feeds have an optional return_ member, and an optional finished_ member
+                Member return_member = new Member(getRettype(), "return_");
+                return_member.addAnnotation(m_context, new Annotation(m_context.getAnnotationDeclaration("optional")));
+                out_type.addMember(return_member);
+                Member finished_member = new Member(m_context.createPrimitiveTypeCode(Kind.KIND_BOOLEAN), "finished_");
+                finished_member.addAnnotation(m_context, new Annotation(m_context.getAnnotationDeclaration("optional")));
+                out_type.addMember(finished_member);
+            }
+            else
+            {
+                // Add output parameters as members
+                getParameters().forEach(param -> {
+                    if (param.isOutput())
+                    {
+                        Member member = new Member(param.getTypecode(), param.getName());
+                        out_type.addMember(member);
+                    }
+                });
+                // Add return_ member if operation has a return type
+                if (getRettype() != null)
+                {
+                    Member return_member = new Member(getRettype(), "return_");
+                    out_type.addMember(return_member);
+                }
+            }
+
+            m_out_type = out_type;
+        }
+
+        return m_out_type;
+    }
+
+    public StructTypeCode getResultTypeCode()
+    {
+        if (m_result_type == null)
+        {
+            Interface parent = (Interface)getParent();
+            String scope = parent.getHasScope() ? parent.getScope() + "::detail" : "detail";
+            StructTypeCode result_type = new StructTypeCode(
+                scope,
+                parent.getName() + "_" + getName() + "_Result");
+
+            // The result type should be a `@choice`, which means that it should have MUTABLE extensibility
+            result_type.get_extensibility(ExtensibilityKind.MUTABLE);
+
+            // Add out type as a member of the result type
+            Member result_member = new Member(getOutTypeCode(), "result");
+            result_member.addAnnotation(m_context, new Annotation(m_context.getAnnotationDeclaration("optional")));
+            result_member.addAnnotation(m_context, new Annotation(m_context.getAnnotationDeclaration("hashid")));
+            result_type.addMember(result_member);
+
+            // Add exceptions as optional members
+            getExceptions().forEach(exception -> {
+                String member_name = exception.getFormatedScopedname() + "_ex";
+                Member member = new Member(((Exception)exception).getTypeCode(), member_name);
+                member.addAnnotation(m_context, new Annotation(m_context.getAnnotationDeclaration("optional")));
+                member.addAnnotation(m_context, new Annotation(m_context.getAnnotationDeclaration("hashid")));
+                result_type.addMember(member);
+            });
+
+            m_result_type = result_type;
+        }
+
+        return m_result_type;
+    }
+
+    private Context m_context = null;
+    private StructTypeCode m_in_type = null;
+    private StructTypeCode m_out_type = null;
+    private StructTypeCode m_result_type = null;
 }
