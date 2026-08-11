7a644cb072124959436c4d2f0d1385a0bed3027b
Ricardo González Moreno <ricardo@richiware.dev>
2026-08-11 09:14:09 +0200

    Add testing `-python`
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- src/test/java/com/eprosima/fastdds/FastDDSGenTest.java
+++ src/test/java/com/eprosima/fastdds/FastDDSGenTest.java
@@ -210,13 +210,38 @@ public class FastDDSGenTest
                 "share/fastddsgen/java/fastddsgen",
                 INPUT_PATH,
                 OUTPUT_PATH,
-                "CMake",
+                "-example CMake",
                 list_tests,
                 blacklist_tests);
         tests.addCMakeArguments("-DCMAKE_BUILD_TYPE=Debug");
         tests.removeTests("basic_inner_types");
 
-        boolean testResult = tests.runTests();
+        java.util.List<String> python_blacklist_tests = new java.util.ArrayList<>();
+        if (blacklist_tests != null)
+        {
+            python_blacklist_tests.addAll(blacklist_tests);
+        }
+        python_blacklist_tests.add("constants"); // swig not support constants with the same name in different modules
+        python_blacklist_tests.add("declarations"); // swig not support structures with the same name in different modules
+        python_blacklist_tests.add("enumerations"); // swig not support enumerations with the same name in different modules
+        python_blacklist_tests.add("exceptions"); // swig not support structures with the same name in different modules
+        python_blacklist_tests.add("external");
+        python_blacklist_tests.add("inner_const_and_typedef"); // swig not support constants with the same name in different modules
+        python_blacklist_tests.add("interfaces"); // swig not support structures with the same name in different modules
+        python_blacklist_tests.add("modules"); // swig not support structures with the same name in different modules
+
+        TestManager python_tests = new TestManager(
+                TestLevel.COMPILE,
+                "share/fastddsgen/java/fastddsgen",
+                INPUT_PATH,
+                OUTPUT_PATH,
+                "-python",
+                list_tests,
+                python_blacklist_tests);
+        python_tests.addCMakeArguments("-DCMAKE_BUILD_TYPE=Debug");
+        python_tests.removeTests("basic_inner_types");
+
+        boolean testResult = tests.runTests() && python_tests.runTests();
         assertEquals(true, testResult);
     }
 }
